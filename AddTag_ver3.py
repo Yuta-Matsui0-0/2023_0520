a8229987556e9b6ad79a87d0b3526fd7dc7df796
Yuta-Matsui0-0 <113221481+Yuta-Matsui0-0@users.noreply.github.com>
2023-06-16 11:55:43 +0900

    AddTag_ver3.py を更新

--- AddTag_ver3.py
+++ AddTag_ver3.py
@@ -7,22 +7,19 @@ import pandas as pd
 
 def add_tag_after_heading(doc, heading, tag, filename):
     tag_added = False
-    for i in range(len(doc.paragraphs)):
-        paragraph = doc.paragraphs[i]
-        if 'Heading' in paragraph.style.name and paragraph.text == heading:
-            # Add a new paragraph with the tag after the heading
-            new_paragraph = doc.add_paragraph(tag)
-            # Move the new paragraph to be after the heading
-            doc.element.body.insert(i+1, new_paragraph._element)
-            tag_added = True
+    heading_indices = [i for i, para in enumerate(doc.paragraphs) if 'Heading' in para.style.name and para.text == heading]
+    for i in heading_indices:
+        new_paragraph = doc.add_paragraph(tag)
+        doc.element.body.insert(i+1, new_paragraph._element)
+        tag_added = True
     if tag_added:
         if filename not in tag_added_files:
             tag_added_files[filename] = []
-        tag_added_files[filename].append(tag)
+        tag_added_files[filename].append(heading)
 
 def export_to_excel(tag_added_files):
     writer = pd.ExcelWriter(os.path.join(output_folder, 'Tag_Added_Report.xlsx'), engine='openpyxl')
-    for filename in os.listdir(input_folder):
+    for filename in tag_added_files:
         data = {"Heading": headings, "Tag Added": []}
         for heading in headings:
             if filename in tag_added_files and heading in tag_added_files[filename]:
@@ -33,28 +30,23 @@ def export_to_excel(tag_added_files):
         df.to_excel(writer, sheet_name=filename, index=False)
     writer.save()
 
-input_folder = 'path/to/Input'
-output_folder = 'path/to/Output'
+input_folder = './Input'
+output_folder = './Output'
 headings = ['ABCD', 'EFGH', 'IJKL']
 tags = ['<ABCD></>', '<EFGH></>', '<IJKL></>']
 tag_added_files = {}
 
-# Generate a new folder with the current timestamp
 current_timestamp = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 output_folder = os.path.join(output_folder, f"{current_timestamp}_AddTag")
 os.mkdir(output_folder)
 
-# Process each Word document in the input folder
 for filename in os.listdir(input_folder):
     if filename.endswith('.docx'):
         file_path = os.path.join(input_folder, filename)
         doc = Document(file_path)
-        # Add the appropriate tag after each heading
         for heading, tag in zip(headings, tags):
             add_tag_after_heading(doc, heading, tag, filename)
-        # Save the updated document to the output folder
         output_file_path = os.path.join(output_folder, filename[:-5] + '_AddTag.docx')
         doc.save(output_file_path)
 
-# Export the results to Excel
 export_to_excel(tag_added_files)
